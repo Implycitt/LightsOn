b097aa21cc16d3160c1e6596ecb8a31812f2a0a6
Implycitt <qgbordelon@gmail.com>
2024-05-19 19:07:45 -0500

    SDL2 window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use sdl2::event::Event;
+use crate::winsdl::Winsdl;
+
+mod winsdl;
+
 fn main() {
-    println!("Hello, world!");
+    let mut winsdl = Winsdl::new(800, 600).unwrap();
+
+    'running: loop {
+        for event in winsdl.event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'running,
+                _ => { }
+            }
+        }
+
+        unsafe {
+            gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        
+        winsdl.window.gl_swap_window();
+    }
 }
